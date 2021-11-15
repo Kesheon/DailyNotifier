231d5b6a930412fde2e100134743ed5e9ba33bd0
Trey McCullough <treymccullough17@gmail.com>
2021-11-15 15:18:28 -0600

    variable changes

--- DailyNotifications.py
+++ DailyNotifications.py
@@ -9,13 +9,12 @@ from pushbullet import Pushbullet
 Scripture1Date = date.fromisoformat('2022-01-01')
 if(Scripture1Date == date.today()):
     API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
-    Scripture1File = "DailyText.txt"
-    with open(Scripture1File, mode='r') as f:
-        Scripture1Text = f.read()
+    file = "DailyText.txt"
+    with open(file, mode='r') as f:
+        text = f.read()
     pb = Pushbullet(API_KEY)
-    push = pb.push_note('Daily Text', Scripture1Text)
+    push = pb.push_note('Daily Text', text)
 
-    
 #----------------------------------------------------------
 # 01/02/2022 (pick a scripture) is Scripture2
 #date.fromisoformat('yyyy-mm-dd')
@@ -23,11 +22,11 @@ if(Scripture1Date == date.today()):
 Scripture2Date = date.fromisoformat('2022-01-02')
 if(Scripture2Date == date.today()):
     API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
-    Scripture2File = "DailyText.txt"
-    with open(Scripture2File, mode='r') as f:
-        Scripture2Text = f.read()
+    file = "DailyText.txt"
+    with open(file, mode='r') as f:
+        text = f.read()
     pb = Pushbullet(API_KEY)
-    push = pb.push_note('Daily Text', Scripture2Text)
+    push = pb.push_note('Daily Text', text)
 
 #--------------------------------------------------------
 
