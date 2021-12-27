513515c3b98cd4b82e848a558dad398e90e450a0
Trey McCullough <treymccullough17@gmail.com>
2021-12-27 13:52:51 -0600

    added 5 scriptures

--- DailyNotifications.py
+++ DailyNotifications.py
@@ -1,39 +1,86 @@
 from datetime import date
 from pushbullet import Pushbullet
 
-# Place scriptures on the first line of a txt file only,
-# so that Pytohn doesn't have to read more than one line
-
 #--------------------------------------------------------
 # 01/01/2022
-# (pick a scripture) is Scripture1Date
+# (2 Timothy 3:15) is Scripture1Date
 # date.fromisoformat('yyyy-mm-dd')
 
 Scripture1Date = date.fromisoformat('2022-01-01')
 if(Scripture1Date == date.today()):
     API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
-    file = "DailyText.txt"#file names need to be the same as its scripture
+    file = "scripture1.txt"
     with open(file, mode='r') as f:
         text = f.read()
     pb = Pushbullet(API_KEY)
-    push = pb.push_note('Daily Text', text)
-
+    push = pb.push_note('2 Timothy 3:15', text)
+    
+    file2 = "scripture1Continued.txt"
+    with open(file2, mode='r') as f2:
+        text2 = f2.read()
+    pb = Pushbullet(API_KEY)
+    push = pb.push_note('continued', text2)
+    
 #----------------------------------------------------------
 # 01/02/2022
-# (pick a scripture) is Scripture2Date
+# (Revelation 9:5) is Scripture2Date
 # date.fromisoformat('yyyy-mm-dd')
 
 Scripture2Date = date.fromisoformat('2022-01-02')
 if(Scripture2Date == date.today()):
     API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
-    file = "DailyText.txt"#file names need to be the same as its scripture
+    file = "scripture2.txt"
     with open(file, mode='r') as f:
         text = f.read()
     pb = Pushbullet(API_KEY)
-    push = pb.push_note('Daily Text', text)
+    push = pb.push_note('Revelation 9:5', text)
 
 #--------------------------------------------------------
 
+#----------------------------------------------------------
+# 01/03/2022
+# (Proverbs 15:3) is Scripture3Date
+# date.fromisoformat('yyyy-mm-dd')
 
+Scripture3Date = date.fromisoformat('2022-01-03')
+if(Scripture3Date == date.today()):
+    API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
+    file = "scripture3.txt"
+    with open(file, mode='r') as f:
+        text = f.read()
+    pb = Pushbullet(API_KEY)
+    push = pb.push_note('Proverbs 15:3', text)
 
+#--------------------------------------------------------
 
+#----------------------------------------------------------
+# 01/04/2022
+# (2 Timothy 4:7) is Scripture4Date
+# date.fromisoformat('yyyy-mm-dd')
+
+Scripture4Date = date.fromisoformat('2021-01-04')
+if(Scripture4Date == date.today()):
+    API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
+    file = "scripture4.txt"
+    with open(file, mode='r') as f:
+        text = f.read()
+    pb = Pushbullet(API_KEY)
+    push = pb.push_note('2 Timothy 4:3', text)
+
+#--------------------------------------------------------
+
+#----------------------------------------------------------
+# 01/05/2022
+# (Ephesians 6:13) is Scripture5Date
+# date.fromisoformat('yyyy-mm-dd')
+
+Scripture5Date = date.fromisoformat('2021-12-27')
+if(Scripture5Date == date.today()):
+    API_KEY = "o.LqGwAtH8QWLr5KyqYeYCHgxXElxhMkGn"
+    file = "scripture5.txt"
+    with open(file, mode='r') as f:
+        text = f.read()
+    pb = Pushbullet(API_KEY)
+    push = pb.push_note('Ephesians 6:13', text)
+
+#--------------------------------------------------------
